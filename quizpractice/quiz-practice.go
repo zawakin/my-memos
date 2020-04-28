bbecdb7f7dd6e08925874664f54793b033cc80ee
zawawahoge <zawawabis@gmail.com>
2020-04-29 00:42:05 +0900

    walk all markdowns

--- quizpractice/quiz-practice.go
+++ quizpractice/quiz-practice.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 // QuizPractice is interface for quiz-practice.
@@ -26,25 +27,30 @@ func New() QuizPractice {
 }
 
 func (qp *quizPractice) Run(cxt context.Context) error {
-	mds, err := qp.loadMarkdowns()
+	mds, err := qp.loadMarkdowns("computer-architecture")
 	if err != nil {
 		return err
 	}
 
 	for _, md := range mds {
 		fmt.Println(md.Path)
-		for i, line := range md.Contents {
-			if i < 5 {
-				fmt.Println(line)
+		outliners := md.ToOutliners()
+		for _, o := range outliners {
+			fmt.Println(o.Level, o.Name, len(o.Contents))
+			for i, content := range o.Contents {
+				if i > 0 {
+					continue
+				}
+				fmt.Println(i, content)
 			}
 		}
-
 	}
 
 	return nil
 }
 
-func (qp *quizPractice) loadMarkdowns() ([]*Markdown, error) {
+// LoadMarkdowns loads all markdowns walking in all the sub directories of `path`.
+func LoadMarkdowns(path string) ([]*Markdown, error) {
 	var mds []*Markdown
 	wk := func(path string, info os.FileInfo, err error) error {
 		if filepath.Ext(path) == ".md" {
@@ -57,7 +63,7 @@ func (qp *quizPractice) loadMarkdowns() ([]*Markdown, error) {
 		}
 		return nil
 	}
-	err := filepath.Walk(".", wk)
+	err := filepath.Walk(path, wk)
 	if err != nil {
 		return nil, err
 	}
@@ -101,4 +107,59 @@ func (md *Markdown) Load() error {
 
 	md.Contents = contents
 	return nil
+}
+
+// Outliner represents markdown component.
+type Outliner struct {
+	Name     string
+	Level    int
+	Contents []string
+}
+
+// ToOutliners creates outliners by parsing the contents.
+func (md *Markdown) ToOutliners() []*Outliner {
+	isOutline := regexp.MustCompile(`^(#+) +(.*)$`)
+
+	outliners := make([]*Outliner, 0)
+	var outliner *Outliner
+	for _, line := range md.Contents {
+		if line == "" {
+			continue
+		}
+		ss := isOutline.FindStringSubmatch(line)
+		if len(ss) == 3 {
+			// Outline
+			outliner = &Outliner{
+				Name:     ss[2],
+				Level:    len(ss[1]),
+				Contents: []string{},
+			}
+			outliners = append(outliners, outliner)
+			continue
+		}
+		if outliner == nil {
+			// Skip if outliner hasn't been created yet.
+			continue
+		}
+		outliner.Contents = append(outliner.Contents, line)
+	}
+	return outliners
+}
+
+// Node is struct of node.
+type Node struct {
+	Depth    int
+	Name     string
+	Contents []string
+	Children []*Node
+	Parent   *Node
+}
+
+// NewNode is constructor of node.
+func NewNode(depth int, name string, parent *Node) *Node {
+	return &Node{
+		Depth:  depth,
+		Name:   name,
+		Parent: parent,
+	}
 }
